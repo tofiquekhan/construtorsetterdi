a54c246535b4c293846b34f27a388d4c8b8e3a90
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2022-01-10 22:55:32 +0530

    Setter Method Dependency Injection

--- src/myproject/constructorsetterdi/beans/Student.java
+++ src/myproject/constructorsetterdi/beans/Student.java
@@ -6,11 +6,41 @@ public class Student {
 	private String sname;
 	private String saddr;
 	
-	public Student(String sid,String sname,String saddr) {
+	/*
+	 * public Student(String sid,String sname,String saddr) { this.sid = sid;
+	 * this.sname = sname; this.saddr = saddr; System.out.
+	 * println("--------------------Constructor Dependency Injection -----------------------"
+	 * ); }
+	 */
+	
+	
+
+	public String getSid() {
+		
+		return sid;
+	}
+
+	public void setSid(String sid) {
+		System.out.println("--------------Setter Method Dependency Injection----------------");
 		this.sid = sid;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
 		this.sname = sname;
+	}
+
+	public String getSaddr() {
+		return saddr;
+	}
+
+	public void setSaddr(String saddr) {
 		this.saddr = saddr;
 	}
+
 	
 	public void getStudentDetails() {
 		System.out.println("Student Detials");
@@ -19,5 +49,4 @@ public class Student {
 		System.out.println("Student Name 	: "+sname);
 		System.out.println("Student Address : "+saddr);
 	}
-
 }
